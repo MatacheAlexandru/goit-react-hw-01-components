6a1bab9f69f84bbc411fa602ec3a624b628830d2
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-08-21 09:43:56 +0300

    commit test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   base: "/goit-react-hw-01-components/", // Înlocuiește cu numele repo-ului tău
-  // Alte setări de configurare Vite
-};
+  plugins: [react()],
+});
